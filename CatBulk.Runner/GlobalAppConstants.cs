460238e70dab9af1ebafd10aacf98997a05ccd99
UchiTesting <56003633+UchiTesting@users.noreply.github.com>
2026-02-27 02:07:37 +0100

    Add simple parameter management (#2)
    * Add a set of constants for string messages
    
    * Initiate simple parameter management
    
    * Fix RegEx to accept connection string syntax

---
+++ CatBulk.Runner/GlobalAppConstants.cs
@@ -0,0 +1,19 @@
+﻿namespace CatBulk.Runner
+{
+    internal static class GlobalAppConstants
+    {
+        internal static class Infrastructure
+        {
+            public const string DefaultMasterConnectionString = "Server=(localdb)\\MSSQLLocalDB;Integrated Security=true;";
+            public const string DefaultOperationalConnectionString = "Server=(localdb)\\MSSQLLocalDB;Integrated Security=true;Initial Catalog=CatBulkDb;Pooling=true;Connect Timeout=30;";
+        }
+        internal static class Messages
+        {
+            internal static class File
+            {
+                public const string NotFound = "FILE NOT FOUND EXCEPTION. THE WORLD GONNA EXPLODE ! :O ";
+                public const string WrongFormat = "WRONG FILE FORMAT EXCEPTION. THE WORLD GONNA EXPLODE ! :O ";
+            }
+        }
+    }
+}
